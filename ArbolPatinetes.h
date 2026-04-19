02929a35f596876a3c87a99c7f5f2c61c5b57d9b
franciscoperezmjl <franciscoperezmjl@gmail.com>
2026-04-19 19:36:40 +0200

    Add files via upload

---
+++ ArbolPatinetes.h
@@ -0,0 +1,31 @@
+#ifndef ARBOLPATINETES_H
+#define ARBOLPATINETES_H
+
+#include "Patinete.h"
+
+struct Nodo {
+    Patinete data;
+    Nodo* izquierda;
+    Nodo* derecha;
+    
+    Nodo(const Patinete& p) : data(p), izquierda(nullptr), derecha(nullptr) {} %la & hace referencia y const es para no modificar pero recibirlo
+};
+
+class ArbolPatinetes {
+private:
+    Nodo* raiz; %primer nodo
+    
+    void insertarRecursivo(Nodo*& nodo, const Patinete& p); %mete patienetes
+    Patinete* buscarRecursivo(Nodo* nodo, int id); %si coincide se queda, sino baja a la izq o der
+    void mostrarInordenRecursivo(Nodo* nodo); %muestra el Inorden
+    void liberarMemoria(Nodo* nodo);
+
+public:
+    ArbolPatinetes();
+    ~ArbolPatinetes();
+    void insertar(const Patinete& p); %añade patientes al arbol
+    Patinete* buscar(int id); %busca patienetes por id
+    void mostrarInorden();
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "Patinete.h"
+#include "ArbolPatinetes.h"
+
+int main() {
+    ArbolPatinetes arbol;
+    
+    std::cout << "=== Plataforma de consulta de patinetes ===" << std::endl;
+    std::cout << "\nDatos de prueba" << std::endl;
+    
+    arbol.insertar(Patinete(105, "disponible"));
+    arbol.insertar(Patinete(87, "mantenimiento"));
+    arbol.insertar(Patinete(123, "ocupado"));          %USE ESTOS DE EJEMPLO PUES ANDO ALGO PERDIDO DE LA LISTA ESTA
+    arbol.insertar(Patinete(45, "disponible"));
+    arbol.insertar(Patinete(156, "disponible"));
+    arbol.insertar(Patinete(92, "mantenimiento"));
+    arbol.insertar(Patinete(78, "ocupado"));
+    arbol.insertar(Patinete(134, "disponible"));
+    
+    arbol.mostrarInorden();
+    
+    std::cout << "\n=== Ejemplos de Búsqueda ===" << std::endl;
+    
+    Patinete* resultado1 = arbol.buscar(123);
+    if (resultado1 != nullptr) { %si lo encuentra muestra esto
+        std::cout << "Búsqueda exitosa - Patinete ID 123: Estado = " << resultado1->estado << std::endl; 
+    } else {
+        std::cout << "Patinete ID 123 no encontrado." << std::endl;
+    }
+    
+    Patinete* resultado2 = arbol.buscar(999); %este no lo encuentra entonces nos lo dice
+    if (resultado2 != nullptr) {
+        std::cout << "Búsqueda exitosa - Patinete ID 999: Estado = " << resultado2->estado << std::endl;
+    } else {
+        std::cout << "Patinete ID 999 no encontrado." << std::endl;
+    }
+    
+    return 0;
+}
