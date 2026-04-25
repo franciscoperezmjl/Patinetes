eabb5c59762906664a47127a3be3f90298d967e3
franciscoperezmjl <franciscoperezmjl@gmail.com>
2026-04-25 19:30:43 +0200

    Cambie errores tontos

--- Parte 2/ArbolPatinetes.cpp
+++ Parte 2/ArbolPatinetes.cpp
@@ -1,5 +1,5 @@
 #include "ArbolPatinetes.h"
-#include <iostream> %para el cout
+#include <iostream> // para el cout
 
 ArbolPatinetes::ArbolPatinetes() {
     raiz = nullptr;
@@ -21,11 +21,11 @@ void ArbolPatinetes::insertar(const Patinete& p) {
     insertarRecursivo(raiz, p);
 }
 
-void ArbolPatinetes::insertarRecursivo(Nodo*& nodo, const Patinete& p) { %Puntero* referencia&
+void ArbolPatinetes::insertarRecursivo(Nodo*& nodo, const Patinete& p) { // Puntero* referencia&
     if (nodo == nullptr) {
         nodo = new Nodo(p);
     } else if (p.id < nodo->data.id) {
-        insertarRecursivo(nodo->izquierda, p); %si es mayor el nodo para la izq, sino a la der, asi hasta llegar al id buscado
+        insertarRecursivo(nodo->izquierda, p); // si es mayor el nodo para la izq, sino a la der, asi hasta llegar al id buscado
     } else if (p.id > nodo->data.id) {
         insertarRecursivo(nodo->derecha, p);
     } else {
@@ -33,14 +33,14 @@ void ArbolPatinetes::insertarRecursivo(Nodo*& nodo, const Patinete& p) { %Punter
     }
 }
 
-Patinete* ArbolPatinetes::buscar(int id) { %empieza desde la raiz
+Patinete* ArbolPatinetes::buscar(int id) { // empieza desde la raiz
     return buscarRecursivo(raiz, id);
 }
 
-Patinete* ArbolPatinetes::buscarRecursivo(Nodo* nodo, int id) { %recorre arbol
-    if (nodo == nullptr) { %no existe
+Patinete* ArbolPatinetes::buscarRecursivo(Nodo* nodo, int id) { // recorre arbol
+    if (nodo == nullptr) { // no existe
         return nullptr;
-    } else if (id == nodo->data.id) { %devuelve patienete
+    } else if (id == nodo->data.id) { // devuelve patienete
         return &nodo->data;
     } else if (id < nodo->data.id) {
         return buscarRecursivo(nodo->izquierda, id);
@@ -49,16 +49,17 @@ Patinete* ArbolPatinetes::buscarRecursivo(Nodo* nodo, int id) { %recorre arbol
     }
 }
 
-void ArbolPatinetes::mostrarInorden() { %muestra patinetes
+void ArbolPatinetes::mostrarInorden() { // muestra patinetes
     std::cout << "Patinetes en orden (ID - Estado):" << std::endl;
     mostrarInordenRecursivo(raiz);
     std::cout << std::endl;
 }
 
-void ArbolPatinetes::mostrarInordenRecursivo(Nodo* nodo) { %recorre por oden el arbol mostrando los ID ordenados
+void ArbolPatinetes::mostrarInordenRecursivo(Nodo* nodo) { // recorre por oden el arbol mostrando los ID ordenados
     if (nodo != nullptr) {
         mostrarInordenRecursivo(nodo->izquierda);
         std::cout << "ID: " << nodo->data.id << " - Estado: " << nodo->data.estado << std::endl;
         mostrarInordenRecursivo(nodo->derecha);
     }
 }
+
