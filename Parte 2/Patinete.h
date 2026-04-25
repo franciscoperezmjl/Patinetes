b22967cfd5f7fade50d99a387f592cdedc8f80db
franciscoperezmjl <franciscoperezmjl@gmail.com>
2026-04-25 19:32:19 +0200

    mas errores de c

--- Parte 2/Patinete.h
+++ Parte 2/Patinete.h
@@ -5,9 +5,9 @@
 
 class Patinete {
 public:
-    int id; %id del patinete
-    std::string estado; %guarda el estado del patinete
-    Patinete(int id, std::string estado) : id(id), estado(estado) {} %meto id y estado
+    int id; // id del patinete
+    std::string estado; // guarda el estado del patinete
+    Patinete(int id, std::string estado) : id(id), estado(estado) {} // meto id y estado
 };
 
 #endif
