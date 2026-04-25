07af5b1d25b2f9145318e1685ef6998c44f1a072
franciscoperezmjl <franciscoperezmjl@gmail.com>
2026-04-25 19:31:52 +0200

    quite errores tontos

--- Parte 2/main.cpp
+++ Parte 2/main.cpp
@@ -10,7 +10,7 @@ int main() {
     
     arbol.insertar(Patinete(105, "disponible"));
     arbol.insertar(Patinete(87, "mantenimiento"));
-    arbol.insertar(Patinete(123, "ocupado"));          %USE ESTOS DE EJEMPLO PUES ANDO ALGO PERDIDO DE LA LISTA ESTA
+    arbol.insertar(Patinete(123, "ocupado"));          // USE ESTOS DE EJEMPLO PUES ANDO ALGO PERDIDO DE LA LISTA ESTA
     arbol.insertar(Patinete(45, "disponible"));
     arbol.insertar(Patinete(156, "disponible"));
     arbol.insertar(Patinete(92, "mantenimiento"));
@@ -19,18 +19,18 @@ int main() {
     
     arbol.mostrarInorden();
     
-    std::cout << "\n=== Ejemplos de Búsqueda ===" << std::endl;
+    std::cout << "\n=== Ejemplos de Busqueda ===" << std::endl; //los busqueda no llevan acento porq se ralla al ejecutarlo
     
     Patinete* resultado1 = arbol.buscar(123);
-    if (resultado1 != nullptr) { %si lo encuentra muestra esto
-        std::cout << "Búsqueda exitosa - Patinete ID 123: Estado = " << resultado1->estado << std::endl; 
+    if (resultado1 != nullptr) { // si lo encuentra muestra esto
+        std::cout << "Busqueda exitosa - Patinete ID 123: Estado = " << resultado1->estado << std::endl; 
     } else {
         std::cout << "Patinete ID 123 no encontrado." << std::endl;
     }
     
-    Patinete* resultado2 = arbol.buscar(999); %este no lo encuentra entonces nos lo dice
+    Patinete* resultado2 = arbol.buscar(999); // este no lo encuentra entonces lo dice
     if (resultado2 != nullptr) {
-        std::cout << "Búsqueda exitosa - Patinete ID 999: Estado = " << resultado2->estado << std::endl;
+        std::cout << "Busqueda exitosa - Patinete ID 999: Estado = " << resultado2->estado << std::endl;
     } else {
         std::cout << "Patinete ID 999 no encontrado." << std::endl;
     }
