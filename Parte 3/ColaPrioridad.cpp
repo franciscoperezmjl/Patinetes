598b0494c3bb0476e6d410bbd2eb3299cf158812
Hugo Fernández Cartea <hugo.fernandez2.01@uie.edu>
2026-04-27 22:24:20 +0200

    Comentarios de Helpers

--- Parte 3/ColaPrioridad.cpp
+++ Parte 3/ColaPrioridad.cpp
@@ -1,13 +1,31 @@
 #include "ColaPrioridad.h"
 #include <iostream>
 
-// ── helpers ──────────────────────────────────────────────
-static inline int padre(int i){ return (i-1)/2; }
-static inline int hijoIzq(int i){ return 2*i+1; }
-static inline int hijoDer(int i){ return 2*i+2; }
-static inline void swap(Incidencia& a, Incidencia& b){ Incidencia t=a; a=b; b=t; }
 
-// O(log N): compara con padre y sube mientras gravedad sea menor
+// La estructura normal de un árbol se "aplasta" en una lista simple
+// Se necesita usar matemática para saber dónde está la familia de cada nodo
+// ¿cómo se navega por el árbol si no hay punteros?, Con un mapeo matemático de los índices del array 
+
+// ── HELPERS (GPS MATEMÁTICO DEL ARBOL) ──────────────────────────────────────────────
+
+// Si un nodo está en la posición 'i', su padre siempre está en la posición (i-1)/2
+static  int padre(int i){ return (i-1)/2; }
+
+// Si un nodo está en la posición 'i', su hijo izquierdo está en la posición (2*i)+1
+static  int hijoIzq(int i){ return 2*i+1; }
+
+// Si un nodo está en la posición 'i', su hijo derecho está en la posición (2*i)+2
+static  int hijoDer(int i){ return 2*i+2; }
+
+// función que intercambia dos averías de posición
+//& para poder modificar directamente los elementos originales dentro de la lista cuando hacemos el intercambio.
+static  void swap(Incidencia& a, Incidencia& b){ Incidencia t=a; a=b; b=t; } 
+// no se puede hacer directamente a=b para cambiar la posicion, 
+// porque perdemos el valor de a para luego pasarlo donde estaba b
+
+
+
+// O(log N): compara con padre y sube mientras gravedad del padre sea menor
 void ColaPrioridad::flotar(int i){
     while(i>0 && heap[i].gravedad < heap[padre(i)].gravedad){
         swap(heap[i], heap[padre(i)]);
@@ -15,7 +33,7 @@ void ColaPrioridad::flotar(int i){
     }
 }
 
-// O(log N): compara con el hijo menor y baja mientras gravedad sea mayor
+// O(log N): compara con el hijo menor y baja mientras gravedad del hijo sea mayor
 void ColaPrioridad::hundir(int i){
     while(hijoIzq(i) < sz){
         int menor = hijoIzq(i);
@@ -30,9 +48,9 @@ void ColaPrioridad::hundir(int i){
 // ── público ───────────────────────────────────────────────
 bool ColaPrioridad::estaVacia(){ return sz == 0; }
 
-void ColaPrioridad::encolar(int id, std::string desc, int grav){
+void ColaPrioridad::encolar(int id, int grav){
     if(sz >= MAX_HEAP){ std::cout << "Heap lleno\n"; return; }
-    heap[sz++] = Incidencia(id, desc, grav);
+    heap[sz++] = Incidencia(id, grav);
     flotar(sz-1);                          // O(log N)
 }
 
@@ -51,7 +69,8 @@ void ColaPrioridad::mostrar(){
     ColaPrioridad tmp = *this;
     while(!tmp.estaVacia()){
         Incidencia inc = tmp.desencolar();
-        std::cout << "[P" << inc.idPatinete << "] g=" << inc.gravedad
-                  << " | " << inc.descripcion << "\n";
+        std::cout << "[P" << inc.idPatinete << "] g=" << inc.gravedad << "\n";
+                  
     }
 }
+
